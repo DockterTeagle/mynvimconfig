fdd160978d6a87f99fc890f90f97635d6ca676b0
Christopher Dockter <chris.r.dockter@gmail.com>
2023-10-26 10:29:01 -0500

    about to remove tex lab

--- plugins.lua
+++ plugins.lua
@@ -9,33 +9,6 @@ local plugins = {
     lazy = false,
   },
   {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    lazy = false,
-    build = "make",
-  },
-  {
-    "junegunn/fzf",
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end
-  },
-
-  {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = false,
@@ -43,21 +16,9 @@ local plugins = {
     },
   },
   {
-    "saecki/crates.nvim",
-    ft = {"rust","toml"},
-    config = function (_,opts)
-      local crates = require('crates')
-      crates.setup(opts)
-      crates.show()
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local M = require "plugins.configs.cmp"
-      table.insert(M.sources,{name="crates"})
-      return M
-    end
+    "nvim-telescope/telescope-fzf-native.nvim",
+    -- lazy = false,
+    build = "make",
   },
   {
     "lervag/vimtex",
@@ -82,8 +43,55 @@ local plugins = {
     end,
   },
   {
-    "latex-lsp/texlab",
-    ft = "latex,"
+    "junegunn/fzf",
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+
+  {
+    "saecki/crates.nvim",
+    ft = {"rust","toml"},
+    config = function (_,opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources,{name="crates"})
+      return M
+    end
+  },
+  -- {
+  --   "latex-lsp/texlab",
+  --   ft = "latex,"
+  -- },
+  {
+    "ludovicchabant/vim-gutentags",
+    lazy = false,
+  },
+  {
+    "neomake/neomake",
+    lazy = false,
   },
   {
     "rafamadriz/friendly-snippets",
@@ -154,6 +162,7 @@ local plugins = {
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
     opts = overrides.treesitter,
   },
 
