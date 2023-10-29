bde97f05c6a949635dcf445fb8921a178128fc77
Christopher Dockter <chris.r.dockter@gmail.com>
2023-10-29 18:36:54 -0500

    removed some comments and started debugging slow python startup

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -7,39 +7,22 @@ local b = require("null-ls").builtins
   local sources = {
   -- Code actions
   b.code_actions.gitsigns,
-  -- b.code_actions.eslint_d,
   -- Completion
   b.completion.luasnip,
 
   -- diagnostics
-  -- b.diagnostics.cpplint,
-  -- b.diagnostics.clj_kondo,
-  -- b.diagnostics.eslint_d,
-  -- b.diagnostics.flake8,
-  -- b.diagnostics.selene,
-  -- b.diagnostics.statix,
-  -- b.diagnostics.stylelint,
-  -- b.diagnostics.shellcheck,
-  b.diagnostics.ruff,
-  b.diagnostics.mypy.with({
-    extra_args = function()
-      local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-      return { "--python-executable", virtual .. "/bin/python3" }
-    end,
-  }),
+  -- b.diagnostics.ruff,
+  -- b.diagnostics.mypy.with({
+  --   extra_args = function()
+  --     local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+  --     return { "--python-executable", virtual .. "/bin/python3" }
+  --   end,
+  -- }),
   b.diagnostics.vale,
 
   -- Formatters
-  -- b.formatting.astyle,
-  -- b.formatting.alejandra,
   b.formatting.black,
   b.formatting.bibtextidy,
-  -- b.formatting.dart_format,
-  -- b.formatting.prettierd,
-  -- b.formatting.rustfmt,
-  -- b.formatting.stylua,
-  -- b.formatting.yamlfmt,
-  -- b.formatting.zprint,
   b.formatting.clang_format,
   }
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
