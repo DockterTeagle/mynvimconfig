776aeedc724710b26b554e133d70ee817f75ae32
Christopher Dockter <chris.r.dockter@gmail.com>
2023-11-06 10:06:18 -0600

    changed from dmypy cause it wasnt analyzing correctly

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -20,42 +20,42 @@ local sources = {
   },
   d.mypy.with {
     prefer_local = "/home/christopher/anaconda3/bin",
-    command = "dmypy",
-    args = function(params)
-      local t1 = {
-        "run",
-        "--timeout",
-        "5000000",
-        "--",
-        "--hide-error-context",
-        "--no-color-output",
-        "--show-absolute-path",
-        "--show-column-numbers",
-        "--show-error-codes",
-        "--no-error-summary",
-        "--no-pretty",
-        "--cache-fine-grained",
-        "--sqlite-cache",
-        "--shadow-file",
-        params.bufname,
-        params.temp_path,
-        params.bufname,
-      }
-      local t2 = vim.lsp.buf.list_workspace_folders()
-      for _, v in ipairs(t2) do
-        table.insert(t1, v)
-      end
-      return t1
-    end,
-    timeout = 500000000,
-    -- Do not run in fugitive windows, or when inside of a .venv area
-    runtime_condition = function(params)
-      if string.find(params.bufname, "fugitive") or string.find(params.bufname, ".venv") then
-        return false
-      else
-        return true
-      end
-    end,
+    -- command = "dmypy",
+    -- args = function(params)
+    --   local t1 = {
+    --     "run",
+    --     "--timeout",
+    --     "5000000",
+    --     "--",
+    --     "--hide-error-context",
+    --     "--no-color-output",
+    --     "--show-absolute-path",
+    --     "--show-column-numbers",
+    --     "--show-error-codes",
+    --     "--no-error-summary",
+    --     "--no-pretty",
+    --     "--cache-fine-grained",
+    --     "--sqlite-cache",
+    --     "--shadow-file",
+    --     params.bufname,
+    --     params.temp_path,
+    --     params.bufname,
+    --   }
+    --   local t2 = vim.lsp.buf.list_workspace_folders()
+    --   for _, v in ipairs(t2) do
+    --     table.insert(t1, v)
+    --   end
+    --   return t1
+    -- end,
+    -- timeout = 500000000,
+    -- -- Do not run in fugitive windows, or when inside of a .venv area
+    -- runtime_condition = function(params)
+    --   if string.find(params.bufname, "fugitive") or string.find(params.bufname, ".venv") then
+    --     return false
+    --   else
+    --     return true
+    --   end
+    -- end,
     -- extra_args = function()
     --   local virtual = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX" or "/usr"
     --   return { "--python-executable", virtual .. "/bin/python3" }
