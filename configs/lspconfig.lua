0d022524b2720aace885e47f68671c93f5fa9c0f
Christopher Dockter <chris.r.dockter@gmail.com>
2023-11-17 18:01:02 -0600

    added a function(?) to fix pyright being monstrously slow

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,9 +1,15 @@
+local ok, wf = pcall(require, "vim.lsp._watchfiles")
+if ok then
+  wf._watchfunc = function()
+    return function() end
+  end
+end
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 -- local util = require "lspconfig/util"
--- local servers = { "clangd", "pyright", "texlab" }
-local servers = { "clangd", "texlab" }
+local servers = { "clangd", "pyright", "texlab" }
+-- local servers = { "clangd", "texlab" }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
