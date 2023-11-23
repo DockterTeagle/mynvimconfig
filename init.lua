bfb31f12cd4d7853a358c2eb415135afff10ef4a
Christopher Dockter <chris.r.dockter@gmail.com>
2023-11-23 14:35:20 -0600

    removed providers as it was not needed I believe

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
+-- local enable_providers = {
+--   "python3_provider",
+--   "node_provider",
+--   -- and so on
+-- }
+-- for _, plugin in pairs(enable_providers) do
+--   vim.g["loaded_" .. plugin] = nil
+--   vim.cmd("runtime " .. plugin)
+-- end
 vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/LuaSnip"
-local enable_providers = {
-  "python3_provider",
-  "node_provider",
-  -- and so on
-}
-for _, plugin in pairs(enable_providers) do
-  vim.g["loaded_" .. plugin] = nil
-  vim.cmd("runtime " .. plugin)
-end
 vim.cmd [[
 " Use Tab to expand and jump through snippets
 imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
