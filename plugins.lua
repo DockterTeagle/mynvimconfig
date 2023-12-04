960c2b5b975b7effb94d13e32c69e10c3413ef9b
christopher Dockter <chris.r.dockter@gmail.com>
2023-12-03 23:11:43 -0600

    fixed fzf error

--- plugins.lua
+++ plugins.lua
@@ -13,6 +13,16 @@ local plugins = {
     end,
   },
   {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension "fzf"
+      end,
+    },
+  },
+  {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     cmd = "Trouble",
