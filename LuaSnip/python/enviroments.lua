a040af4d28afd3fc33bb90c50ef753a6e449dd80
christopher Dockter <chris.r.dockter@gmail.com>
2024-01-31 14:54:22 -0600

    made the main snippet correct for python

--- LuaSnip/python/enviroments.lua
+++ LuaSnip/python/enviroments.lua
@@ -1,6 +1,6 @@
 local helpers = require "custom.LuaSnip.helper-functions"
 return {
   helpers.s({ trig = "main", snippetType = "snippet" }, {
-    helpers.t 'if __name__ == "main":',
+    helpers.t 'if __name__ == "__main__":',
   }, { condition = helpers.line_begin }),
 }
