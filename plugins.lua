f57cc1f6499f0cd0836dd0580eec3e744e581749
christopher Dockter <flywire.husked978@passinbox.com>
2024-02-10 12:18:04 -0600

    renamed folke to Folke to make it easier to access

--- plugins.lua
+++ plugins.lua
@@ -1,6 +1,6 @@
 local overrides = require "custom.configs.overrides"
 local cmp = require "cmp"
-local folke = require "custom.configs.folke"
+local folke = require "custom.configs.Folke.folke"
 local plugins = {
   --begin Rocky linux suggestions:
   -- {
