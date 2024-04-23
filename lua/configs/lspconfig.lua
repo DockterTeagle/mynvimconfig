6b3459445d470a91c034927d9534a2e0110f4baf
Dr.Teagle <chrisdockter@proton.me>
2024-04-23 02:31:48 -0500

    debugging

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,29 +1,29 @@
-local ok, wf = pcall(require, "vim.lsp._watchfiles")
-if ok then
-	wf._watchfunc = function()
-		return function() end
-	end
-end
+-- local ok, wf = pcall(require, "vim.lsp._watchfiles")
+-- if ok then
+-- 	wf._watchfunc = function()
+-- 		return function() end
+-- 	end
+-- end
 --this was the command that fixed pyright being slow for me above
-local lspconfig = require("lspconfig")
-require("noice").setup()
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-local servers = { "clangd", "pyright", "texlab", "marksman", "cmake", "bashls", "denols", "vimls", "julials", "nil_ls" }
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-	})
-end
-require("neodev").setup({
-	library = {
-		plugins = {
-			"nvim-dap-ui",
-		},
-		types = true,
-	},
-})
+-- local lspconfig = require("lspconfig")
+-- require("noice").setup()
+-- local on_attach = require("nvchad.configs.lspconfig").on_attach
+-- local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- local servers = { "clangd", "pyright", "texlab", "marksman", "cmake", "bashls", "denols", "vimls", "julials", "nil_ls" }
+-- for _, lsp in ipairs(servers) do
+-- 	lspconfig[lsp].setup({
+-- 		on_attach = on_attach,
+-- 		capabilities = capabilities,
+-- 	})
+-- end
+-- require("neodev").setup({
+-- 	library = {
+-- 		plugins = {
+-- 			"nvim-dap-ui",
+-- 		},
+-- 		types = true,
+-- 	},
+-- })
 -- elseif vim.g.isNixOS then
 -- 	lspconfig.clangd.setup({
 -- 		cmd = { "clangd" },
