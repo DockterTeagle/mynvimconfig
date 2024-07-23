e8b28f452953bd059bb3d7d40efa7f0a9ebc7b6c
Dr.Teagle <chrisdockter@proton.me>
2024-07-23 01:27:29 -0500

    feat: removed unneeded, and non-working os check

--- init.lua
+++ init.lua
@@ -13,58 +13,23 @@ vim.opt.rtp:prepend(lazypath)
 
 local lazy_config = require("configs.lazy")
 local overrides = require("configs.overrides")
-vim.g.isNixOS = os.execute("test -e /etc/nixos")
-if not vim.g.isNixOS then
-	require("lazy").setup({
-		{
-			"NvChad/NvChad",
-			lazy = false,
-			branch = "v2.5",
+require("lazy").setup({
+	{
+		"NvChad/NvChad",
+		lazy = false,
+		branch = "v2.5",
 
-			import = "nvchad.plugins",
+		config = function()
+			require("options")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		enabled = false,
+	},
 
-			config = function()
-				require("options")
-			end,
-		},
-		{
-			"williamboman/mason.nvim",
-			opts = overrides.mason,
-		},
-		{
-			"jay-babu/mason-nvim-dap.nvim",
-			ft = { "c", "cpp", "rust" },
-			dependencies = {
-				"williamboman/mason.nvim",
-				"mfussenegger/nvim-dap",
-			},
-			opts = {
-				handlers = {},
-			},
-		},
-
-		{ import = "plugins" },
-	}, lazy_config)
-elseif vim.g.isNixOS then
-	-- load plugins
-	require("lazy").setup({
-		{
-			"NvChad/NvChad",
-			lazy = false,
-			branch = "v2.5",
-
-			config = function()
-				require("options")
-			end,
-		},
-		{
-			"williamboman/mason.nvim",
-			enabled = false,
-		},
-
-		{ import = "plugins" },
-	}, lazy_config)
-end
+	{ import = "plugins" },
+}, lazy_config)
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
