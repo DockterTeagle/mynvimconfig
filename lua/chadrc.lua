9aaf2370322f2929c0430b922c2134f9b1956b51
Dr.Teagle <chrisdockter@proton.me>
2024-10-01 16:24:15 -0500

    removed unused git signs config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,10 +1,16 @@
 ---@type ChadrcConfig
 local M = {}
 
+M.nvdash = {
+	load_on_startup = true,
+}
 M.ui = {
 	theme = "tokyonight",
 }
+M.colorify = {
+	enabled = true,
+}
 M.lsp = {
-  signature = true,
+	signature = true,
 }
 return M
