058c4d1d314d42b0aadb104aa62ff7fd58fd956b
Dr.Teagle <chrisdockter@proton.me>
2024-10-01 16:28:53 -0500

    found problem line causing rust analyzer to not work

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,21 +1,3 @@
-local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-lsp_capabilities.textDocument.completion.completionItem = {
-	documentationFormat = { "markdown", "plaintext" },
-	snippetSupport = true,
-	preselectSupport = true,
-	insertReplaceSupport = true,
-	labelDetailsSupport = true,
-	deprecatedSupport = true,
-	commitCharactersSupport = true,
-	tagSupport = { valueSet = { 1 } },
-	resolveSupport = {
-		properties = {
-			"documentation",
-			"detail",
-			"additionalTextEdits",
-		},
-	},
-}
 local lsp_on_attach = require("configs.lspconfigDefaults").on_attach
 vim.g.rustaceanvim = {
 	tools = {
@@ -25,7 +7,6 @@ vim.g.rustaceanvim = {
 	},
 	server = {
 		on_attach = lsp_on_attach,
-		capabilities = lsp_capabilities,
 		default_settings = {
 			["rust-analyzer"] = {
 				cargo = {
--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -25,14 +25,14 @@ return {
 			"hrsh7th/nvim-cmp",
 		},
 		ft = { "rust", "toml" },
-		config = function(_, opts)
-			local crates = require("crates")
-			crates.setup(opts)
-			require("cmp").setup.buffer({
-				sources = { { name = "crates" } },
-			})
-			crates.show()
-		end,
+		-- config = function(_, opts)
+		-- 	local crates = require("crates")
+		-- 	crates.setup(opts)
+		-- 	require("cmp").setup.buffer({
+		-- 		sources = { { name = "crates" } },
+		-- 	})
+		-- 	crates.show()
+		-- end,
 	},
 	{
 		"rust-lang/rust.vim",
