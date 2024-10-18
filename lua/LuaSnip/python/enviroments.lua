9a4aabaf589df33f9fc1d81ba686724585376f51
Dr.Teagle <chrisdockter@proton.me>
2024-10-18 16:26:40 -0500

    changed main to an auto snippet

--- lua/LuaSnip/python/enviroments.lua
+++ lua/LuaSnip/python/enviroments.lua
@@ -1,6 +1,6 @@
 local helpers = require("LuaSnip.helper-functions")
 return {
-	helpers.s({ trig = "main", snippetType = "snippet" }, {
+	helpers.s({ trig = "main", snippetType = "autosnippet" }, {
 		helpers.t('if __name__ == "__main__":'),
 	}, { condition = helpers.line_begin }),
 }
