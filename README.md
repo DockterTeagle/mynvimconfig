c7681476473cc8f04d67de2ae0f0156944db83f8
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-15 16:39:35 -0600

    updated README, flake and flake.lock

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # My NeoVim configuration
 Based on `NvChad`
 # TODO:
-1. fix rustacean error on rust ft
-2. remove all nvchad dependencies
-3. switch over to kickstart.nvim
-4. fix all nvim errors
-5. add all the documentation from kickstart
-8. make tmux keys work in oil
-10. get trouble config working
+- [ ] fix rustacean error on rust ft
+- [ ] remove all nvchad dependencies
+- [ ] switch over to kickstart.nvim
+- [ ] fix all nvim errors
+- [ ] add all the documentation from kickstart
+- [ ] make tmux keys work in oil
+- [ ] get trouble config working
+- [ ] go to blink.cmp
--- flake.nix
+++ flake.nix
@@ -4,25 +4,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;}
-    {
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      perSystem = {pkgs, ...}: let
-        runtimeDeps = with pkgs; [
-          lua
-          gcc
-          nixd
-          clang
-        ];
-        nvim =
-          pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped
-          (pkgs.neovimUtils.makeNeovimConfig
-            {
+      perSystem =
+        { pkgs, ... }:
+        let
+          runtimeDeps = with pkgs; [
+            lua
+            gcc
+            nixd
+            clang
+          ];
+          nvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
+            pkgs.neovimUtils.makeNeovimConfig {
               customRc = ''
                 set runtimepath^=${./.}
                 source ${./.}/init.lua
@@ -35,23 +35,23 @@
                 ":"
                 "${pkgs.lib.makeBinPath runtimeDeps}"
               ];
-            });
-      in {
-        # overlays = {
-        #   neovim = _:__prev: {
-        #     neovim = nvim;
-        #   };
-        #   default = self'.overlays.neovim;
-        # };
-        packages = rec {
-          neovim = nvim;
-          default = neovim;
-        };
-        devShells.default =
-          pkgs.mkShell
-          {
+            }
+          );
+        in
+        {
+          # overlays = {
+          #   neovim = _:__prev: {
+          #     neovim = nvim;
+          #   };
+          #   default = self'.overlays.neovim;
+          # };
+          packages = rec {
+            neovim = nvim;
+            default = neovim;
+          };
+          devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
-              lua
+              lua5_4
               stylua
               selene
               lua-language-server
@@ -60,6 +60,6 @@
               clang
             ];
           };
-      };
+        };
     };
 }
