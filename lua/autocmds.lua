d02f0ac21563f7f8b4549af343d03dc6c68f79bc
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-30 14:55:06 -0600

    added autocmd for linters that doesnt work as I dont have access to the status line I think

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require("nvchad.autocmds")
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	callback = function()
+		local linters = require("lint").get_running()
+		if #linters == 0 then
+			return "󰦕"
+		end
+		return "󱉶 " .. table.concat(linters, ", ")
+	end,
+})
