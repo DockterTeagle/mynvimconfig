29ac4694ffd57f017ef3dd543c4e0ca2d334e96d
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-30 22:08:44 -0600

    removed unnecessary variables

--- lua/plugins/FolkePlugins/folkePlugins.lua
+++ lua/plugins/FolkePlugins/folkePlugins.lua
@@ -1,7 +1,3 @@
-local noice = require("plugins.FolkePlugins.noiceMain")
-local todoComments = require("plugins.FolkePlugins.TodoCommentsMain")
-local trouble = require("plugins.FolkePlugins.troubleMain")
-local wk = require("plugins.FolkePlugins.which-key")
 return {
 	{
 		"folke/lazydev.nvim",
@@ -12,13 +8,7 @@ return {
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 				-- Or relative, which means they will be resolved from the plugin dir.
 				"lazy.nvim",
-				-- Only load the lazyvim library when the `LazyVim` global is found
-				-- Load the wezterm types when the `wezterm` module is required
-				-- Needs `justinsgithub/wezterm-types` to be installed
-				-- Load the xmake types when opening file named `xmake.lua`
-				-- Needs `LelouchHe/xmake-luals-addon` to be installed
 			},
-			-- disable when a .luarc.json file is found
 			enabled = function(root_dir)
 				return (not vim.uv.fs_stat(root_dir .. "/.luarc.json"))
 					or vim.g.lazydev_enabled == nil and true
@@ -27,10 +17,9 @@ return {
 		},
 	},
 	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-	-- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
-	noice,
-	wk,
-	todoComments,
-	trouble,
+	require("plugins.FolkePlugins.TodoCommentsMain"),
 	require("plugins.FolkePlugins.flash"),
+	require("plugins.FolkePlugins.noiceMain"),
+	require("plugins.FolkePlugins.troubleMain"),
+	require("plugins.FolkePlugins.which-key"),
 }
