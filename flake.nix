e5d57c371e233e20a51cab1e402efbcd514c510c
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-30 22:01:11 -0600

    added luarc

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         let
           pkgs = import inputs.nixpkgs {
             inherit system;
-            # overlays = [ inputs.gen-luarc.overlays.default ];
+            overlays = [ inputs.gen-luarc.overlays.default ];
           };
           runtimeDeps = with pkgs; [
             lua
@@ -74,54 +74,54 @@
                     lualine-nvim
                     luasnip
                     mini-icons
-                    "noice.nvim"
-                    "lua5.1-nui.nvim" # NUI
-                    "nvim-lint"
-                    "nvim-notify"
-                    "luajit2.1-rustaceanvim"
-                    "lua5.1-telescope.nvim"
-                    "todo-comments.nvim"
-                    "tokyonight.nvim"
-                    "vim-easy-align"
-                    "vim-tmux-navigator"
-                    "vimtex"
-                    "which-key.nvim"
-                    "aerial.nvim"
-                    "comment.nvim"
-                    "compiler.nvim"
-                    "conform.nvim"
-                    "crates.nvim"
-                    "diffview.nvim"
-                    "lua5.1-gitsigns.nvim"
-                    "FixCursorHold.nvim"
-                    "inc-rename.nvim"
-                    "indent-blankline.nvim"
-                    "luvit-meta"
-                    "nlsp-settings.nvim"
-                    "nvim-autopairs"
-                    "nvim-colorizer.lua"
-                    "nvim-dap"
-                    "nvim-dap-python"
-                    "nvim-dap-ui"
-                    "nvim-dap-virtual-text"
-                    "nvim-spectre"
+                    noice-nvim
+                    nui-nvim
+                    nvim-lint
+                    nvim-notify
+                    rustaceanvim
+                    telescope-nvim
+                    telescope-frecency-nvim
+                    tokyonight-nvim
+                    vim-easy-align
+                    vim-tmux-navigator
+                    vimtex
+                    which-key-nvim
+                    which-key-nvim
+                    aerial-nvim
+                    comment-nvim
+                    compiler-nvim
+                    conform-nvim
+                    crates-nvim
+                    diffview-nvim
+                    gitsigns-nvim
+                    FixCursorHold-nvim
+                    inc-rename-nvim
+                    indent-blankline-nvim
+                    luvit-meta
+                    nlsp-settings-nvim
+                    nvim-autopairs
+                    nvim-colorizer-lua
+                    nvim-dap
+                    nvim-dap-python
+                    nvim-dap-ui
+                    nvim-dap-virtual-text
+                    nvim-spectre
                     nvim-treesitter
                     oil-nvim
                     overseer-nvim
-                    "refactoring.nvim"
-                    "rust.vim"
-                    "telescope-dap.nvim"
-                    "telescope-frecency.nvim"
-                    "telescope-fzf-native.nvim"
-                    "vim-grammarous"
+                    refactoring-nvim
+                    rust-vim
+                    telescope-dap-nvim
+                    telescope-fzf-native-nvim
+                    vim-grammarous
                   ];
                 };
 
               in
-              # ln -fs ${luarc} .luarc.json
               # bash
               ''
                 ${self'.checks.pre-commit-check.shellHook}
+                ln -fs ${luarc} .luarc.json
               '';
             inputsFrom = [ inputs'.blink.devShells.default ];
             packages = with pkgs; [
