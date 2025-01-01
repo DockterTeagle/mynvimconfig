ad6fcb65093a76e7aa883afe88c74488b2f4917c
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-31 18:07:26 -0600

    made lazydev into its own file

---
+++ lua/plugins/FolkePlugins/lazydev.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- Library paths can be absolute
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				-- Or relative, which means they will be resolved from the plugin dir.(not vim.uv.fs_stat(root_dir .. "/.luarc.json"))
+				"lazy.nvim",
+				{ "snacks.nvim", words = { "snacks" } },
+			},
+			enabled = function(root_dir)
+				return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
+			end,
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+}
