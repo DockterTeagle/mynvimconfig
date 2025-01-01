b057df5064abb9b3646adbf78a554d6c1b8c7157
Dr.Teagle <steampowered.mom596@passinbox.com>
2024-12-31 18:25:43 -0600

    removed unnecessary rust plugins

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -23,29 +23,4 @@ return {
 			})
 		end,
 	},
-	{
-		"saecki/crates.nvim",
-		dependencies = {
-			"hrsh7th/nvim-cmp",
-		},
-		ft = { "rust", "toml" },
-		-- config = function(_, opts)
-		-- 	local crates = require("crates")
-		-- 	crates.setup(opts)
-		-- 	require("cmp").setup.buffer({
-		-- 		sources = { { name = "crates" } },
-		-- 	})
-		-- 	crates.show()
-		-- end,
-		-- vim.keymap.set("n", "<leader>rcu", function()
-		-- 	require("crates").upgrade_all_crates()
-		-- end, { desc = "update crates" })
-	},
-	{
-		"rust-lang/rust.vim",
-		ft = "rust",
-		init = function()
-			vim.g.rustfmt_autosave = 1
-		end,
-	},
 }
