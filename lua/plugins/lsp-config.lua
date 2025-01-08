a189a40463a70ae4abb24e3ac46645f096cab30d
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-07 18:01:49 -0600

    nlsp-settings to be required

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,11 +2,13 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		event = "User FilePost",
+		dependencies = {
+			{
+				"tamago324/nlsp-settings.nvim",
+			},
+		},
 		config = function()
 			require("configs.lsp.lspconfig")
 		end,
-	},
-	{
-		"tamago324/nlsp-settings.nvim",
 	},
 }
