8098669eb929154a57b4729459c36517c4213622
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-09 17:11:35 -0600

    updated various things

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,50 +7,35 @@ return {
 			"tamago324/nlsp-settings.nvim",
 			{ "antosha417/nvim-lsp-file-operations", config = true },
 		},
-		opts = function()
-			return require("configs.lsp.lspconfig")
-		end,
 		config = function(_, opts)
-			local servers = opts
-			local lspconfig = require("lspconfig")
-			local on_attach = require("configs.lsp.lspconfigDefaults").on_attach
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities() and require("blink.cmp").get_lsp_capabilities()
-			)
-			for server, server_opts in pairs(opts.servers) do
-				-- Retrieve the filetypes associated with the server (if any)
-				local filetypes = lspconfig[server].document_config
-						and lspconfig[server].document_config.default_config.filetypes
-					or {}
-
-				-- If the server has filetypes, set up an autocmd to trigger LSP setup for the filetypes
-				if #filetypes > 0 then
-					for _, ft in ipairs(filetypes) do
-						vim.api.nvim_create_autocmd("FileType", {
-							pattern = ft,
-							callback = function()
-								lspconfig[server].setup(vim.tbl_deep_extend("force", {
-									on_attach = on_attach,
-									capabilities = capabilities,
-								}, server_opts))
-							end,
-						})
-					end
-				else
-					-- If no filetypes are specified, set up the LSP for all filetypes
-					vim.api.nvim_create_autocmd("BufReadPost", {
-						pattern = "*",
-						callback = function()
-							lspconfig[server].setup(vim.tbl_deep_extend("force", {
-								on_attach = on_attach,
-								capabilities = capabilities,
-							}, server_opts))
-						end,
-					})
-				end
-			end
+			require("configs.lsp.lspconfig")
+			-- local servers = opts.servers
+			-- local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+			-- local has_blink, blink = pcall(require, "blink.cmp")
+			-- local capabilities = vim.tbl_deep_extend(
+			-- 	"force",
+			-- 	{},
+			-- 	vim.lsp.protocol.make_client_capabilities() and require("blink.cmp").get_lsp_capabilities()
+			-- )
+			-- local function setup(server)
+			-- 	local server_opts = vim.tbl_deep_extend("force", {
+			-- 		capabilities = vim.deepcopy(capabilities),
+			-- 	}, servers[server] or {})
+			-- 	if server_opts.enabled == false then
+			-- 		return
+			-- 	end
+			--
+			-- 	if opts.setup[server] then
+			-- 		if opts.setup[server](server, server_opts) then
+			-- 			return
+			-- 		end
+			-- 	elseif opts.setup["*"] then
+			-- 		if opts.setup["*"](server, server_opts) then
+			-- 			return
+			-- 		end
+			-- 	end
+			-- 	require("lspconfig")[server].setup(server_opts)
+			-- end
 		end,
 	},
 }
