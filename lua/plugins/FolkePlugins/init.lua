d7731a40f1f0a2e7e9d4c5ee80504790594a4a73
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-09 22:11:30 -0600

    added init.lua

---
+++ lua/plugins/FolkePlugins/init.lua
@@ -0,0 +1,10 @@
+return {
+	require("plugins.FolkePlugins.noiceMain"),
+	require("plugins.FolkePlugins.flash"),
+	require("plugins.FolkePlugins.TodoCommentsMain"),
+	require("plugins.FolkePlugins.troubleMain"),
+	require("plugins.FolkePlugins.snacks"),
+	require("plugins.FolkePlugins.which-key"),
+	require("plugins.FolkePlugins.edgy"),
+	require("plugins.FolkePlugins.lazydev"),
+}
