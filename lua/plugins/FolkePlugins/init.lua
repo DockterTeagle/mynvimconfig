3d1cb0602e703e904c2579018a848f30a161c101
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-09 22:56:01 -0600

    fix init.lua concers

--- lua/plugins/FolkePlugins/init.lua
+++ lua/plugins/FolkePlugins/init.lua
@@ -1,10 +1,12 @@
-return {
-	require("plugins.FolkePlugins.noiceMain"),
-	require("plugins.FolkePlugins.flash"),
-	require("plugins.FolkePlugins.TodoCommentsMain"),
-	require("plugins.FolkePlugins.troubleMain"),
-	require("plugins.FolkePlugins.snacks"),
-	require("plugins.FolkePlugins.which-key"),
-	require("plugins.FolkePlugins.edgy"),
-	require("plugins.FolkePlugins.lazydev"),
-}
+local plugins = {}
+local plugin_dir = vim.fn.stdpath("config") .. "/lua/plugins/FolkePlugins/"
+
+-- Loop through all Lua files in the directory
+for _, file in ipairs(vim.fn.glob(plugin_dir .. "*.lua", false, true)) do
+	local plugin_name = file:match("([^/]+)%.lua$")
+	if plugin_name and plugin_name ~= "init" then
+		table.insert(plugins, require("plugins.FolkePlugins." .. plugin_name))
+	end
+end
+
+return plugins
