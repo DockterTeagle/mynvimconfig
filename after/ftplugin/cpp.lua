f322140832a60ba38ace032f032c41f796806391
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-11 16:08:38 -0600

    figured out buffers(kindof)

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source/Header" })
+vim.keymap.set("n", "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source/Header", buffer = true })
