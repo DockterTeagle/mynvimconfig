57d7e6237275a063fdecfaa28e2c27d5944cac08
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-27 21:49:45 -0600

    applied formatting

--- lua/LuaSnip/helper-functions.lua
+++ lua/LuaSnip/helper-functions.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 -- Be sure to explicitly define these LuaSnip node abbreviations!
-local ls = require "luasnip"
+local ls = require("luasnip")
 local s = ls.snippet
 local sn = ls.snippet_node
 local t = ls.text_node
@@ -14,34 +14,34 @@ local rep = require("luasnip.extras").rep
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 local tex_utils = {}
 tex_utils.in_mathzone = function() -- math context detection
-  return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+	return vim.fn["vimtex#syntax#in_mathzone"]() == 1
 end
 tex_utils.in_text = function()
-  return not tex_utils.in_mathzone()
+	return not tex_utils.in_mathzone()
 end
 tex_utils.in_comment = function() -- comment detection
-  return vim.fn["vimtex#syntax#in_comment"]() == 1
+	return vim.fn["vimtex#syntax#in_comment"]() == 1
 end
 tex_utils.in_env = function(name) -- generic environment detection
-  local is_inside = vim.fn["vimtex#env#is_inside"](name)
-  return (is_inside[1] > 0 and is_inside[2] > 0)
+	local is_inside = vim.fn["vimtex#env#is_inside"](name)
+	return (is_inside[1] > 0 and is_inside[2] > 0)
 end
 -- A few concrete environments---adapt as needed
 tex_utils.in_itemize = function() -- itemize environment detection
-  return tex_utils.in_env "itemize"
+	return tex_utils.in_env("itemize")
 end
 tex_utils.in_tikz = function() -- TikZ picture environment detection
-  return tex_utils.in_env "tikzpicture"
+	return tex_utils.in_env("tikzpicture")
 end
 tex_utils.in_enumerate = function()
-  return tex_utils.in_env "enumerate"
+	return tex_utils.in_env("enumerate")
 end
 tex_utils.get_visual = function(args, parent)
-  if #parent.snippet.env.LS_SELECT_RAW > 0 then
-    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
-  else -- If LS_SELECT_RAW is empty, return a blank insert node
-    return sn(nil, i(1))
-  end
+	if #parent.snippet.env.LS_SELECT_RAW > 0 then
+		return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+	else -- If LS_SELECT_RAW is empty, return a blank insert node
+		return sn(nil, i(1))
+	end
 end
 M.tex_utils = tex_utils
 M.ls = ls
