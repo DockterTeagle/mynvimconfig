b4ed0edd764bfbfb6fd69bef59b40b20b8f03f76
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-01-30 15:42:53 -0600

    fix mappings

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -2,4 +2,14 @@ local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "<leader>ca", function()
 	vim.cmd.RustLsp("codeAction")
 end, { silent = true, buffer = bufnr })
-vim.keymap.set("n", "J", "<cmd>RustLsp joinLines<CR>", { desc = "better join Lines for rust", remap = true })
+vim.keymap.set("n", "J", function()
+	vim.cmd.RustLsp("joinLines")
+end, { desc = "better join Lines for rust", remap = true })
+vim.keymap.set(
+	"n",
+	"K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+	function()
+		vim.cmd.RustLsp({ "hover", "actions" })
+	end,
+	{ silent = true, buffer = bufnr }
+)
--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -1,2 +1,1 @@
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set("n", "<localleader>vc", "<CMD>VimtexCompile<CR>", { desc = "Start the VimTeX compiler", buffer = bufnr })
