5ec9c7f28497b6f4fa25b927dc86e71af622bfa1
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-02-22 20:25:56 -0600

    change to alejandra and also remove gen-luarc

--- flake.nix
+++ flake.nix
@@ -4,62 +4,53 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
     nixd.url = "github:nix-community/nixd";
   };
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      perSystem =
-        {
-          inputs',
-          self',
-          system,
-          pkgs,
-          ...
-        }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [ inputs.gen-luarc.overlays.default ];
-          };
-          formatter = pkgs.nixfmt-rfc-style;
-          checks = {
-            pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-              src = ./.;
-              hooks = {
-                nixfmt-rfc-style.enable = true;
-                stylua.enable = true;
-              };
+      perSystem = {
+        inputs',
+        self',
+        system,
+        pkgs,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+        checks = {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+              stylua.enable = true;
             };
           };
-          devShells.default = pkgs.mkShell {
-            shellHook =
-              # let
-              #
-              # in
-
-              # bash
-              ''
-                ${self'.checks.pre-commit-check.shellHook}
-              '';
-            packages = with pkgs; [
-              self'.checks.pre-commit-check.enabledPackages
-              inputs'.nixd.packages.nixd
-              stylua
-              selene
-              # lua54Packages.luacheck
-              lua-language-server
-              codespell
-              marksman
-              ltex-ls-plus
-            ];
-          };
         };
+        devShells.default = pkgs.mkShell {
+          shellHook =
+            # let
+            #
+            # in
+            # bash
+            ''
+              ${self'.checks.pre-commit-check.shellHook}
+            '';
+          packages = with pkgs; [
+            self'.checks.pre-commit-check.enabledPackages
+            inputs'.nixd.packages.nixd
+            stylua
+            selene
+            lua-language-server
+            codespell
+            marksman
+            ltex-ls-plus
+            alejandra
+          ];
+        };
+      };
     };
 }
