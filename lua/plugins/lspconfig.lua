2a1d265314a55a8397f5dc85ac24b0e9296fdc22
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-02-24 21:05:52 -0600

    feat: fix lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -33,6 +33,7 @@ return {
 			local lspconfig = require("lspconfig")
 			for server, server_config in pairs(opts.servers) do
 				local config = vim.tbl_deep_extend("force", server_config, {
+					on_attach = require("configs.lsp.lspconfigDefaults").on_attach,
 					capabilities = require("blink.cmp").get_lsp_capabilities(server_config.capabilities),
 				})
 				lspconfig[server].setup(config)
