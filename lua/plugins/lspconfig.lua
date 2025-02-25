a357495860151ee917dc9814b84c7241c6ee6675
Dr.Teagle <steampowered.mom596@passinbox.com>
2025-02-24 18:11:40 -0600

    refactor: blink.cmp and removal of useless comments

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -20,8 +20,6 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
-		-- ft = {},
-		-- event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 		dependencies = {
 			"tamago324/nlsp-settings.nvim",
@@ -32,12 +30,7 @@ return {
 		end,
 		config = function(_, opts)
 			local servers = opts.servers
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				require("blink.cmp").get_lsp_capabilities()
-			)
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 			-- Retrieve the on_attach function
 			local on_attach = require("configs.lsp.lspconfigDefaults").on_attach
@@ -49,27 +42,16 @@ return {
 					on_attach = on_attach, -- Ensure on_attach is passed correctly
 				}, servers[server] or {})
 
-				-- Skip setup if the server is disabled
-				if opts.setup and opts.setup[server] then
-					if opts.setup[server](server, server_opts) then
-						return
-					end
-				elseif opts.setup and opts.setup["*"] then
-					if opts.setup["*"](server, server_opts) then
-						return
-					end
-				end
 				-- Finally, set up the LSP server
 				require("lspconfig")[server].setup(server_opts)
 			end
 
 			-- Loop over each server and call the setup function
 			for server, server_opts in pairs(servers) do
-				if server_opts then
-					-- If the server is enabled, proceed with setup
-					if server_opts.enabled ~= false then
+				if server_opts and server_opts.enabled ~= false then
+					vim.schedule(function()
 						setup(server)
-					end
+					end)
 				end
 			end
 		end,
