4cc1bd11e14f9da072ac7fe37c54a07bdbc37a57
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-06 15:13:46 -0600

    chore: remove unplanned features

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -272,42 +272,6 @@ return {
 		opts = require("configs.lualine").lualine_opts(),
 		dependencies = { "echasnovski/mini.icons" },
 	},
-	--
-	{
-		"nvim-lualine/lualine.nvim",
-		opts = function(_, opts)
-			-- local trouble = require("trouble")
-			-- local symbols = trouble.statusline({
-			-- 	mode = "lsp_document_symbols",
-			-- 	groups = {},
-			-- 	title = false,
-			-- 	filter = { range = true },
-			-- 	format = "{kind_icon}{symbol.name:Normal}",
-			-- 	-- The following line is needed to fix the background color
-			-- 	-- Set it to the lualine section you want to use
-			-- 	hl_group = "lualine_c_normal",
-			-- })
-			-- table.insert(opts.sections.lualine_c, {
-			-- 	symbols.get,
-			-- 	cond = symbols.has,
-			-- })
-			local x = opts.sections.lualine_x
-			for _, comp in ipairs(x) do
-				if comp[1] == "diff" then
-					comp.source = function()
-						local summary = vim.b.minidiff_summary
-						return summary
-							and {
-								added = summary.add,
-								modified = summary.change,
-								removed = summary.delete,
-							}
-					end
-					break
-				end
-			end
-		end,
-	},
 	{
 		"saghen/blink.cmp",
 
