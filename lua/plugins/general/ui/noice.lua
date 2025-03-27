017107eaa5d481234ff10dbf6261afb0903d9866
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-27 08:41:37 -0500

    fix: gixes

--- lua/plugins/general/ui/noice.lua
+++ lua/plugins/general/ui/noice.lua
@@ -73,9 +73,6 @@ return {
 				view = "hover",
 			},
 			-- progress = { enabled = false },
-			messages = {
-				enabled = false,
-			},
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
