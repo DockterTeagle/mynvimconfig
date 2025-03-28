22823d9d7fff44220c54f53747d61987b698025d
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-03-27 22:48:02 -0500

    feat: get ltex plus almost functional

--- lsp/ltexplus.lua
+++ lsp/ltexplus.lua
@@ -1,16 +1,18 @@
-
--- ltex_plus = {
--- 	settings = {
--- 		ltex = {
--- 			-- statusBarItem = true,
--- 			clearDiagnosticsWhenClosingFile = false,
--- 			enabled = true,
--- 			additionalRules = {
--- 				enablePickyRules = true,
--- 				motherTongue = "en-US",
--- 			},
--- 			-- checkFrequency = "save",
--- 			language = "en-US",
--- 		},
--- 	},
--- },
+return {
+	cmd = { "ltex-ls-plus" },
+	on_attach = require("configs.lsp.lspconfigDefaults").on_attach(),
+	capabilities = require("blink.cmp").get_lsp_capabilities(),
+	settings = {
+		ltex = {
+			-- statusBarItem = true,
+			clearDiagnosticsWhenClosingFile = false,
+			enabled = true,
+			additionalRules = {
+				enablePickyRules = true,
+				motherTongue = "en-US",
+			},
+			-- checkFrequency = "save",
+			language = "en-US",
+		},
+	},
+}
