d87a414bd8c1f8cfc5dd4b11f6979ee44052f59d
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-15 15:39:01 -0500

    chore: remove unused check

--- lua/configs/neotest.lua
+++ lua/configs/neotest.lua
@@ -6,11 +6,7 @@ M.opts = {
 	output = { open_on_run = true },
 	quickfix = {
 		open = function()
-			if LazyVim.has("trouble.nvim") then
-				require("trouble").open({ mode = "quickfix", focus = false })
-			else
-				vim.cmd("copen")
-			end
+			require("trouble").open({ mode = "quickfix", focus = false })
 		end,
 	},
 }
