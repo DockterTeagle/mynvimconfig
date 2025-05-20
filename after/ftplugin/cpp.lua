a08424b75dd4d7cb68bf8152f9af7301d29ada4a
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-20 07:42:59 -0500

    chore: remove unused keymap

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -4,4 +4,3 @@ vim.keymap.set(
 	"<cmd>ClangdSwitchSourceHeader<cr>",
 	{ desc = "Switch Source/Header", buffer = true }
 )
-vim.keymap.set("n", "<localleader><F6>", "<CMD>CompilerOpen", { desc = "open the compiler", buffer = true })
