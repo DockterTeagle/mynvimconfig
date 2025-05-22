11df04dda80af0e1ff7732e731ad11fc9be88f1e
Dr.Teagle <65212972+DockterTeagle@users.noreply.github.com>
2025-05-21 19:36:58 -0500

    feat: add markview

--- lua/plugins/ftplugin/markdown.lua
+++ lua/plugins/ftplugin/markdown.lua
@@ -12,23 +12,23 @@ return {
 	-- 	opts = {
 	-- 		formatters = {
 	--
-	-- 			-- ["markdown-toc"] = {
-	-- 			-- 	condition = function(_, ctx)
-	-- 			-- 		for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-	-- 			-- 			if line:find("<!%-%- toc %-%->") then
-	-- 			-- 				return true
-	-- 			-- 			end
-	-- 			-- 		end
-	-- 			-- 	end,
-	-- 			-- },
-	-- 			-- ["markdownlint-cli2"] = {
-	-- 			-- 	condition = function(_, ctx)
-	-- 			-- 		local diag = vim.tbl_filter(function(d)
-	-- 			-- 			return d.source == "markdownlint"
-	-- 			-- 		end, vim.diagnostic.get(ctx.buf))
-	-- 			-- 		return #diag > 0
-	-- 			-- 	end,
-	-- 			-- },
+	-- ["markdown-toc"] = {
+	-- 	condition = function(_, ctx)
+	-- 		for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+	-- 			if line:find("<!%-%- toc %-%->") then
+	-- 				return true
+	-- 			end
+	-- 		end
+	-- 	end,
+	-- },
+	-- ["markdownlint-cli2"] = {
+	-- 	condition = function(_, ctx)
+	-- 		local diag = vim.tbl_filter(function(d)
+	-- 			return d.source == "markdownlint"
+	-- 		end, vim.diagnostic.get(ctx.buf))
+	-- 		return #diag > 0
+	-- 	end,
+	-- },
 	-- 		},
 	-- 		formatters_by_ft = {
 	-- 			-- ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
@@ -38,49 +38,49 @@ return {
 	-- 		},
 	-- 	},
 	-- },
-	{
-		--TODO: get me to work on nixos
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = function()
-			require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-			vim.fn["mkdp#util#install"]()
-		end,
-		keys = {
-			{
-				"<localleader>cp",
-				ft = "markdown",
-				"<cmd>MarkdownPreviewToggle<cr>",
-				desc = "Markdown Preview",
-			},
-		},
-		config = function()
-			vim.cmd([[do FileType]])
-		end,
-	},
 
 	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		branch = "dev",
 		opts = {
-			code = {
-				sign = false,
-				width = "block",
-				right_pad = 1,
-			},
-			heading = {
-				sign = false,
-				icons = {},
-			},
-			checkbox = {
-				enabled = false,
+			icon_provider = "mini",
+			latex = {
+				enable = true,
 			},
 		},
-		config = function(_, opts)
-			require("render-markdown").setup(opts)
-		end,
+		-- dependencies = {
+		-- 	"saghen/blink.cmp",
+		-- },
+	},
+	{
+		"obsidian-nvim/obsidian.nvim",
+		event = {
+			"BufReadPre /home/cdockter/Documents/main/*.md",
+			"BufNewFile /home/cdockter/Documents/main/*.md",
+		},
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
+			"folke/snacks.nvim",
+			-- see above for full list of optional dependencies ☝️
+			"saghen/blink.cmp",
+			"nvim-treesitter/nvim-treesitter",
+			"OXY2DEV/markview.nvim",
+		},
+		---@module 'obsidian'
+		---@type obsidian.config.ClientOpts
+		opts = {
+			picker = {
+				name = "snacks.pick",
+			},
+			completion = {
+				nvim_cmp = false,
+				blink = true,
+			},
+			workspaces = {
+				{ name = "main", path = "~/Documents/main" },
+			},
+		},
 	},
 }
